e5dc34f004de153b098e85861f91f38d4e23b165
Juan R. Loaiza <juanrloaiza@gmail.com>
2021-01-08 08:52:29 -0500

    Updates PDF Parser

--- utils/PDFparser.py
+++ utils/PDFparser.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from spellchecker import SpellChecker
 import glob
 import re
 import json
 import time
-from PyPDF4.pdf import PdfFileReader
+from PyPDF2 import PdfFileReader
 from multiprocessing import Pool
 import pandas as pd
 
 from nltk.corpus import stopwords
-stopwords_es = stopwords.words('spanish') # We load Spanish stopwords.
+stopwords_es = stopwords.words('spanish')  # We load Spanish stopwords.
 
 # We will record data to analyze later.
 data = []
 
-from spellchecker import SpellChecker
-spell = SpellChecker(language=None)  # We will load a custom dictionary from RAE.and HTML files.
+# We will load a custom dictionary from RAE.and HTML files.
+spell = SpellChecker(language=None)
 
 # Note: This version actually includes some manual edits.
 # TODO: Document how these manual edits were produced.
-spell.word_frequency.load_dictionary('../notebooks/wordlists/customDictionary.gz')
+spell.word_frequency.load_dictionary(
+    '../notebooks/wordlists/customDictionary.gz')
 
 # We add dictionaries in English and German
-spell_en = SpellChecker(language = 'en')
-spell_de = SpellChecker(language = 'de')
+spell_en = SpellChecker(language='en')
+spell_de = SpellChecker(language='de')
 
 # And we add a dictionary for manual replacements to do a quicker job.
 with open('../notebooks/wordlists/pdfManualCorrections.json') as fp:
     manualCorrections = json.load(fp)
+
 
 def getCorrectedWord(word):
     """
@@ -44,8 +47,9 @@ def getCorrectedWord(word):
     # There are patterns where spaces went missing but some are
     # easy to detect. Let's try with "filosofía"
     if len(re.findall('\w*filosof[ií]a\w*', word)) > 0:
-        correctedWord = re.sub('(\w*)filosof[íi]a(\w*)', r'\1 filosofía \2', word)
-        print(correctedWord)
+        correctedWord = re.sub(
+            '(\w*)filosof[íi]a(\w*)', r'\1 filosofía \2', word)
+        # print(correctedWord)
         return word, correctedWord
 
     # We will keep track of how long it takes to replace each word.
@@ -55,10 +59,11 @@ def getCorrectedWord(word):
     correctedWord = spell.correction(word)
     #finalTime = time.time() - initialTime
 
-    #if finalTime > 10:
-        #print(f"'{word}' (Sugerencia: {correctedWord})")
+    # if finalTime > 10:
+    #print(f"'{word}' (Sugerencia: {correctedWord})")
 
     return word, correctedWord
+
 
 def processText(doc):
     """
@@ -72,7 +77,8 @@ def processText(doc):
     doc_id = doc['id']
 
     # Let's get all non-stopword words.
-    totalWords = set([word for word in re.findall('\w+', correctedText) if word not in stopwords_es])
+    totalWords = set([word for word in re.findall(
+        '\w+', correctedText) if word not in stopwords_es])
 
     # We take an initial measure of how many words we recognize over the total count.
     initial = len(spell.unknown(totalWords)) / len(totalWords)
@@ -83,42 +89,44 @@ def processText(doc):
     # We eliminate new lines.
     correctedText = correctedText.replace('\n', ' ')
 
-
     # SECOND CORRECTION
     # We replace words that we already know how to replace and that take too
     # long with pyspellshecker.
     for word, replacement in manualCorrections.items():
         correctedText = correctedText.replace(word, replacement)
 
-
     # THIRD CORRECTION
     # There are common artifacts derived from OCR. We can eliminate those manually.
 
     # e.g. A very common pattern is that upper case M is OCR'd as ivi. We can replace
     # with regex. Similar patterns are 1 and l instead of i.
-    correctedText = re.sub(r'([A-Z]+)[li1]v[li1]([A-Z]+)', r'\1M\1', correctedText)
+    correctedText = re.sub(
+        r'([A-Z]+)[li1]v[li1]([A-Z]+)', r'\1M\1', correctedText)
 
     correctedText = re.sub(r'(\w+)[^i]ci[oó]\b', r'\1ción', correctedText)
     correctedText = re.sub(r'\bf[l1Ii](\w+)f[l1Ii]\b', r'\1', correctedText)
 
-
     # FOURTH CORRECTION
-    # We eliminate consonants that are alone and should not be.
-    correctedText = re.sub(r'\b[qwrtpsdfgjklñzxcvbnmQWRTPSDFGJKLÑZXCVBNM]\b', '', correctedText)
+    # We eliminate orphan letters.
+    correctedText = re.sub(
+        r'\b\w\w?\b', '', correctedText, re.VERBOSE)
 
     # FIFTH CORRECTION
     # Use PySpellChecker to get corrected words.
 
     # We take all of the (unique) words in the text.
     words = set(re.findall('\w+', correctedText))
+    print(f"Words to check: {len(words)}")
 
     for word in words:
         if len(word) > 20:
-            print(word)
+            continue
+            # print(word)
 
-    # We must keep the original word as the spell.unknown function passes everything
+    # We must keep the original word as the spell.unknown() function passes everything
     # to lower case, which precludes some replacements later on.
-    unknownWords = [word for word in words if word.lower() in spell.unknown(words)]
+    unknownWords = [word for word in words if word.lower()
+                    in spell.unknown(words)]
 
     # We will consider only unknown words and pass this to the
     # function getting corrected words. We do this in several threads.
@@ -127,13 +135,14 @@ def processText(doc):
 
     # We receive a list of corrections and we implement them.
     for word, correctedWord in correctedWords:
-        correctedText = re.sub(r'\b' + word + r'\b', correctedWord, correctedText)
-
+        correctedText = re.sub(r'\b' + word + r'\b',
+                               correctedWord, correctedText)
 
     # FINISHING STEPS
     # We take the word count again since the text has been modified.
     # Again, we don't count stopwords.
-    totalWords = set([word for word in re.findall('\w+', correctedText) if word not in stopwords_es])
+    totalWords = set([word for word in re.findall(
+        '\w+', correctedText) if word not in stopwords_es])
 
     # We take a final measure of how much of the text we recognize and how long it took.
     correction = len(spell.unknown(totalWords)) / len(totalWords)
@@ -147,10 +156,10 @@ def processText(doc):
 
     # We record our results into the data.
     data.append({'id': doc_id,
-           'initial': initial,
-           'final': correction,
-           'time': timeDelta,
-            'wordCount': len(re.findall('\w+', correctedText))})
+                 'initial': initial,
+                 'final': correction,
+                 'time': timeDelta,
+                 'wordCount': len(re.findall('\w+', correctedText))})
 
     return correctedText
 
@@ -197,7 +206,9 @@ for file in files:
     # Some text only has whitespace characters. It is length > 0 but no meaningful
     # characters. We eliminate those by checking if the text splits into something.
     # If it splits, we process it. Otherwise, we save None as the text value.
-    if text.split():
+
+    # ADD: Let's also only count articles to save time.
+    if text.split() and doc['type'] == 'ARTÍCULOS':
         doc['text'] = processText(doc)
     else:
         doc['text'] = None
