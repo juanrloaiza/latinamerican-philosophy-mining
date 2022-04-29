1a7f7a8927afe80de25d1c3e68c369fc81a9fff0
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-04-28 22:00:26 -0500

    Reimplements notebooks with new objects

---
+++ notebooks/0-collect_data.py
@@ -0,0 +1,26 @@
+from utils.download.scraper import Scraper
+from utils.download.articleparser import ArticleParser
+from utils.filemanager import FileManager
+from utils.registry import Registry
+import os
+
+ARCHIVE_URL = "https://revistas.unal.edu.co/index.php/idval/issue/archive"
+DATA_FOLDER = os.path.abspath("../data")
+REGISTRY_FILE = "utils/article_registry.json"
+DICTIONARIES = "utils/dictionaries"
+
+if not os.path.exists(DATA_FOLDER):
+    os.mkdir(DATA_FOLDER)
+
+registry = Registry(
+    registry_path=REGISTRY_FILE, data_path=DATA_FOLDER, manager=FileManager()
+)
+
+# registry.load_folder()
+# scraper = Scraper(archive_url=ARCHIVE_URL, registry=registry)
+# scraper.scrape()
+
+print("Downloaded all articles! Now let's get parsing...")
+
+parser = ArticleParser(registry=registry, dictionary_path=DICTIONARIES)
+parser.parse()
