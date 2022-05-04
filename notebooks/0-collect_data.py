e510f0b464d8619220ebabfaf3fcb4a86ae8bcb0
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-05-03 20:33:12 -0500

    Minor comments for collection pipeline

--- notebooks/0-collect_data.py
+++ notebooks/0-collect_data.py
@@ -16,11 +16,14 @@ registry = Registry(
     registry_path=REGISTRY_FILE, data_path=DATA_FOLDER, manager=FileManager()
 )
 
+# Loads the current files from the data folder.
 # registry.load_folder()
+
+# Downloads the articles.
 # scraper = Scraper(archive_url=ARCHIVE_URL, registry=registry)
 # scraper.scrape()
+# print("Downloaded all articles! Now let's get parsing...")
 
-print("Downloaded all articles! Now let's get parsing...")
-
+# Parses the downloaded articles.
 parser = ArticleParser(registry=registry, dictionary_path=DICTIONARIES)
 parser.parse()
