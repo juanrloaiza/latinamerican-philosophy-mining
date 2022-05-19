0fa8e4a52970b2cf14481510671c78d4962f44de
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-05-19 08:13:28 -0500

    Fixes a problem in Ghostscript with some pdfs.

--- notebooks/utils/download/parsertools/pdfparser.py
+++ notebooks/utils/download/parsertools/pdfparser.py
@@ -1,6 +1,7 @@
 import ghostscript
 from utils.download.parsertools.wordcorrector import WordCorrector
 from multiprocessing import Process
+import os
 
 
 class PDFParser:
@@ -10,9 +11,15 @@ class PDFParser:
 
     def parse(self, file):
 
-        temp_file = "utils/temp.txt"
+        temp_folder = "utils/temp"
 
-        p = Process(target=self.read, args=(file, temp_file,))
+        p = Process(
+            target=self.read,
+            args=(
+                file,
+                temp_folder,
+            ),
+        )
         print("Starting Ghostscript...")
         p.start()
         p.join(30)
@@ -21,22 +28,37 @@ class PDFParser:
             print("Ghostscript timed out.")
             return None, {"error": True}
 
-        with open(temp_file) as file:
-            full_text = file.read()
+        full_text = ""
+        for path in os.listdir(temp_folder):
+            with open(f"{temp_folder}/{path}") as file:
+                full_text += " " + file.read()
+
+            # We delete the files to avoid having extra pages in the folder for the next file.
+            os.remove(f"{temp_folder}/{path}")
 
         corrector = WordCorrector(dictionary_path=self.dictionary_path)
         new_text, corrector_results = corrector.correct_text(full_text)
 
         return new_text, corrector_results
 
-    def read(self, file, temp_file):
+    def read(self, file, temp_folder):
+        """
+        Extracts text from a PDF file using Ghostscript.
+
+        We define the parameters in a list and pass them to the interface.
+
+        Importantly, we save each page on a separate file using the Ghostscript specification.
+        We will later read each page separately. This avoids some strange behavior from
+        Ghostscript where it only saved the last page of some PDF files.
+        """
 
         args = [
             "-q",
+            "-dNEWPDF",
             "-sDEVICE=txtwrite",
             "-dNOPAUSE",
             "-dSAFER",
-            f'-sOutputFile="{temp_file}"',
+            f'-sOutputFile="{temp_folder}/temp_pg_%d.txt"',
             f'"{file}"',
         ]
 
