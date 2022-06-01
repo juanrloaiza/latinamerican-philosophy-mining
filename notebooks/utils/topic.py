902ef3afe19f96bcb63593149a2034fa9b7b5701
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-06-01 10:24:10 -0500

    Prints probability for article in topic class

--- notebooks/utils/topic.py
+++ notebooks/utils/topic.py
@@ -24,7 +24,7 @@ class Topic:
         """Returns the n top article titles in the topic based on probability."""
         results = []
         for article_id, prob in self.articles:
-            results.append(self.model.get_article_ref(article_id))
+            results.append([self.model.get_article_ref(article_id), prob])
         return results[:n]
 
     def get_top_words(self, n=10, verbose=False):
