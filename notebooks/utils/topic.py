36dd148a12b908d8950b28c8bbed96ffcf2ba18d
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-07-28 10:23:46 -0500

    Optimizes topic population algorithm

--- notebooks/utils/topic.py
+++ notebooks/utils/topic.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from pathlib import Path
 
 
@@ -31,89 +29,45 @@ class Topic:
         # Distributions are in log prob form. Convert to [0, 1] probs.
         self.word_probabilities = np.exp(self.word_probabilities)
 
-        # Sort by mean probability across all time slices
         sorted_idxs = self.word_probabilities.mean(axis=1).argsort()[::-1]
 
-        # Add words until we hit half of the topic's probability mass (0.5/1.0)
-        # or there are 2000 words in the topic (which would lead to eliminating it).
-        checked_probability = 0
-        data = []
-        word_table_raw = {}
-        for idx in sorted_idxs:
+        mean_probabilities = self.word_probabilities.mean(axis=1)
 
-            if checked_probability >= 0.5 or len(word_table_raw) > 2000:
-                break
+        mean_probabilities.sort()
 
-            word = id2word[idx]
-            word_table_raw[word] = []
-            for time_slice, prob in enumerate(self.word_probabilities[idx]):
-                data.append(
-                    {
-                        "Word": word,
-                        "Slice": time_slice,
-                        "Prob": prob,
-                        "Pos": np.where(
-                            self.word_probabilities[:, time_slice].argsort()[::-1]
-                            == idx
-                        )[0][0]
-                        + 1,
-                    }
-                )
+        mean_probabilities = mean_probabilities[::-1]
 
-                word_table_raw[word].append(prob)
+        target_idx = min((mean_probabilities.cumsum() >= 0.5).argmax(), 2000) + 1
 
-            checked_probability += np.mean(self.word_probabilities[idx])
+        topic_words = [id2word[idx] for idx in sorted_idxs[:target_idx]]
 
-        self.full_data = pd.DataFrame(data)
-
-        self.word_table = pd.DataFrame()
-        for word, probs in word_table_raw.items():
-            word_probs = pd.Series(probs, name=word)
-            self.word_table = pd.concat([self.word_table, word_probs], axis=1)
-
-        self.word_table = self.word_table.T
-        self.word_table.insert(0, "Mean prob.", self.word_table.mean(axis=1))
-        self.length = len(self.word_table)
-
-    def top_words(self, n=10) -> pd.Series:
-        """Returns the top n words for the whole topic (averaged across time slices)."""
-        return (
-            self.full_data.groupby("Word")["Prob"]
-            .mean()
-            .sort_values(ascending=False)[:n]
+        self.word_table = pd.DataFrame(
+            self.word_probabilities[sorted_idxs[:target_idx]], index=topic_words
         )
 
-    def get_doc_mass(self, mass: int = 0.5) -> float:
+        self.length = len(self.word_table)
+
+    def top_words(self, n=10) -> np.ndarray:
+        """Returns the top n words for the whole topic (averaged across time slices)."""
+        return self.word_table[:n].index.values
+
+    def get_doc_mass(self, mass: int = 0.5) -> int:
         """Returns the amount of documents that account for `mass` probability mass of a topic."""
 
         # TODO: Use mass parameter
 
         return len(self.docs)
 
-    def get_word_mass(self, mass: int = 0.5) -> float:
+    def get_word_mass(self, mass: int = 0.5) -> int:
         """Returns the amount of words that account for `mass` probability mass of a topic."""
         return self.word_table.shape[0]
 
-    def top_words_evolution_graph(self) -> plt.Axes:
-        """Returns a graph of the evolution of top words per time slice."""
-        df = self.full_data
-
-        ax = sns.pointplot(
-            data=df[df["Pos"] <= 10], x="Slice", y="Pos", hue="Word", palette="tab20"
-        )
-        plt.legend(bbox_to_anchor=(1, 1))
-        plt.ylim(reversed(plt.ylim()))
-        sns.despine()
-
-        return ax
-
     def top_word_evolution_table(self, n=10) -> pd.DataFrame:
         """Returns a DataFrame of top words for each time slice."""
-        slice_lists = pd.DataFrame()
-        for time_slice in range(self.num_slices):
-            df = self.full_data
-            slice_lists[time_slice] = list(
-                df[df["Slice"] == time_slice].sort_values("Pos")["Word"][:n]
+        data = []
+        for time_slice in self.word_table:
+            data.append(
+                self.word_table[time_slice].sort_values(ascending=False)[:n].index
             )
 
-        return slice_lists
+        return pd.DataFrame(data).T
