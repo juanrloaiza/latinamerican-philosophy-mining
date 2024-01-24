256b0e5e50bd603b6dec495fa3f6210b521e5914
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-01-24 12:18:21 -0300

    Minor comment

--- notebooks/utils/model.py
+++ notebooks/utils/model.py
@@ -85,7 +85,8 @@ class Model:
                 f"{idx}:{count}" for idx, count in self.id2word.doc2bow(doc_bow)
             ]
             output_str += (
-                f"{len(self.id2word.doc2bow(doc_bow))} " + " ".join(word_counts) + " \n"
+                f"{len(self.id2word.doc2bow(doc_bow))} " +
+                " ".join(word_counts) + " \n"
             )
 
         with open(models_path / "corpus-mult.dat", "w") as file:
@@ -93,7 +94,8 @@ class Model:
 
         # Get number of documents per time slice
         with open(models_path / "corpus-seq.dat", "w") as file:
-            file.write(f"{self.num_slices}\n" + "\n".join(map(str, self.slices)))
+            file.write(f"{self.num_slices}\n" +
+                       "\n".join(map(str, self.slices)))
 
     def train(self) -> None:
         """Trains the model.
@@ -149,11 +151,14 @@ class Model:
         interface with the topics directly through methods defined in the Topic class.
         """
 
+        # This process is taking a whole minute.
+        # TODO: Check for ways to optimize it besides multithreading.
         if num_workers > 1:
             pool = multiprocessing.Pool(num_workers)
             self.topics = pool.map(self.create_topic, range(self.num_topics))
         else:
-            self.topics = [self.create_topic(i) for i in range(self.num_topics)]
+            self.topics = [self.create_topic(i)
+                           for i in range(self.num_topics)]
 
         # Reads the results from the DTM binary.
         self.classify_documents()
@@ -173,13 +178,13 @@ class Model:
 
     def classify_documents(self) -> None:
         """Classifies documents into the topics in the model."""
-        gamma = np.loadtxt(self.path / "lda-seq" / "gam.dat").reshape(self.num_docs, -1)
+        gamma = np.loadtxt(self.path / "lda-seq" /
+                           "gam.dat").reshape(self.num_docs, -1)
         gamma = gamma / gamma.sum(axis=1, keepdims=True)
 
         normalized_gamma = gamma / gamma.sum(axis=0)
 
         # Classify using likelihood sorting
-
         for topic in self.topics:
             topic.docs = []
             indices = gamma[:, topic.topic_id].argsort()[::-1]
@@ -201,7 +206,8 @@ class Model:
         """Reads the json file with tags, and attaches them
         to the topics."""
         path_to_json_file = (
-            NOTEBOOKS_DIR / "results" / f"topic_tags_{self.num_topics}_{self.seed}.json"
+            NOTEBOOKS_DIR / "results" /
+            f"topic_tags_{self.num_topics}_{self.seed}.json"
         )
 
         if not path_to_json_file.exists():
@@ -242,7 +248,8 @@ class Model:
                 coherence="c_v",
             )
 
-            coherences_per_slice[time_slice] = coherence_model.get_coherence_per_topic()
+            coherences_per_slice[time_slice] = coherence_model.get_coherence_per_topic(
+            )
 
         return coherences_per_slice
 
@@ -347,9 +354,11 @@ Number of trash topics: {len(trash_topic_summaries)}
 
 
 if __name__ == "__main__":
+
     # N_TOPICS = 10
     # base_model = Model(Corpus(registry_path="../utils/article_registry.json"), N_TOPICS)
-    corpus = Corpus(registry_path=NOTEBOOKS_DIR / "utils" / "article_registry.json")
+    corpus = Corpus(registry_path=NOTEBOOKS_DIR /
+                    "utils" / "article_registry.json")
 
     n_topics = 90
     seed = 36775
@@ -357,4 +366,5 @@ if __name__ == "__main__":
     model = Model(corpus, n_topics, seed=seed)
     model.load_topics(num_workers=5)
 
-    model.summarize(NOTEBOOKS_DIR / "results_non_trash.md", omit_trash_topics=True)
+    model.summarize(NOTEBOOKS_DIR / "results_non_trash.md",
+                    omit_trash_topics=True)
